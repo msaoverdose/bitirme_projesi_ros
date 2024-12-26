9b52b6d12c3e40334f9cc5aeb538d9d9375c8200
msaoverdose <msemih12354@gmail.com>
2024-12-26 10:45:33 +0300

    Launch file for launch

--- bitirme_projesi_ros/catkin_ws/src/bitirme/script/navigation_with_web.py
+++ bitirme_projesi_ros/catkin_ws/src/bitirme/script/navigation_with_web.py
@@ -7,12 +7,13 @@ import subprocess
 def start_navigation(req):
     try:
         subprocess.Popen(["rosrun", "bitirme", "dual_reactive.py", "robot_0"])
-        return TriggerResponse(success=True, message="Navigation started.")
+        subprocess.Popen(["rosrun", "bitirme", "dual_reactive.py", "robot_1"])
+        return TriggerResponse(success=True, message="Hareket Başladı.")
     except Exception as e:
         return TriggerResponse(success=False, message=str(e))
 
 if __name__ == '__main__':
     rospy.init_node('navigation_service')
     service = rospy.Service('/start_navigation', Trigger, start_navigation)
-    rospy.loginfo("Navigation service ready.")
+    rospy.loginfo("Harekete Hazır.")
     rospy.spin()
